2ad25780164aa905ecbe0f6f0f041f4835b0955f
Andy <andybarajas0418@gmail.com>
2020-06-01 21:03:28 -0400

    fixed bug where endpoint didnt match expectations

--- data/historical.go
+++ data/historical.go
@@ -24,7 +24,7 @@ type HistoryProvider struct {
 
 func NewHistoryProvider(apiKey, ticker string) *HistoryProvider {
 	t := start // set to 01/06/2020
-	endPoint := BaseUrl + ticker + HistorySuffix + apiKey + startDateSuffix + t
+	endPoint := BaseUrl + ticker + HistorySuffix + apiKey + periodType + frequencyType + startDateSuffix + start
 	return &HistoryProvider{
 		Ticker: ticker,
 		APIKey: apiKey,
@@ -36,11 +36,14 @@ func NewHistoryProvider(apiKey, ticker string) *HistoryProvider {
 // ChangeStartDate changes the start date for the api call , takes in unix time stamp since epoch
 func (hp *HistoryProvider)ChangeStartDate(newTime string){
 	hp.StartDate = newTime
-	hp.EndPoint = BaseUrl + hp.Ticker + HistorySuffix + hp.APIKey + startDateSuffix + hp.StartDate
+	hp.EndPoint = BaseUrl + hp.Ticker + HistorySuffix + hp.APIKey + periodType + frequencyType + startDateSuffix + start
 }
 
 //GetData returns HistoryPayload type
 func (h *HistoryProvider) GetData() *HistoryPayload {
+	if h.EndPoint != "https://api.tdameritrade.com/v1/marketdata/SPXL/pricehistory?apikey=LUL87NNF5RJ3CONF4N3P1MNUUZSOTGNY&periodType=ytd&frequencyType=daily&startDate=1578321000000"{
+		logrus.Infof("\nHAVE: %s\nWANT: %s",h.EndPoint,"https://api.tdameritrade.com/v1/marketdata/SPXL/pricehistory?apikey=LUL87NNF5RJ3CONF4N3P1MNUUZSOTGNY&periodType=ytd&frequencyType=daily&startDate=1578321000000")
+	}
 	var payload HistoryPayload
 	resp, err := http.Get(h.EndPoint)
 	if err != nil {
